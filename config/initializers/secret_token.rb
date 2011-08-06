d2e539c47413eaabeb2db1bbb957830c4b34713a
Dan Lien <dan@Dans-MacBook-Air.local>
2011-08-05 22:59:40 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fridayhacknights::Application.config.secret_token = 'f93c10f984aa3a38dc77454951c517b603374d34f64a5da40dde91042cd3d2f65269b08fcdd8a6c7e6463f22f97d08faedb7b3072b99ca3c289dbd14de3cc379'
