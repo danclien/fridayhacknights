c245240b0b2b6b5c734efa163ff6af856904fc93
Gilbert <gilbertbgarza@gmail.com>
2011-08-06 04:52:41 -0500

    Require owner permissions to create, edit, and destroy projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,15 @@
 class ProjectsController < InheritedResources::Base
+  before_filter :authenticate_hacker!, :except => [:index, :show]
+
+  def show
+    show! do
+      @hacker_is_owner = current_hacker == @project.owner
+    end
+  end
+
+  def create
+    @project = Project.new params[:project]
+    @project.owner = current_hacker
+    create!
+  end
 end
