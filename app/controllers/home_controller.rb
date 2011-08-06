4bb2df52ba5c4a74b55795d7ec298806ba7350e9
Dan Lien <danclien@gmail.com>
2011-08-06 05:31:57 -0500

    Fixed Gemfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @photos = Picasa::Photo.find_all_by_album_id('danclien', '5637559171581951569')[0..5]
+    get_photos
+    @photos = @photos[0..5]
   end
 
 end
