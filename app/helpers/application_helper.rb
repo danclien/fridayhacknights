d48a014d4504ea5ab2e265399e15f65475bc6664
Gilbert <gilbertbgarza@gmail.com>
2011-08-09 03:40:48 -0500

    Holy crap design is addictive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def hacker_list(hackers)
+    render :partial => 'hackers/item-list', :locals => { :hackers => hackers }
+  end
+
+  def project_list(projects)
+    render :partial => 'projects/item-list', :locals => { :projects => projects }
+  end
 end
