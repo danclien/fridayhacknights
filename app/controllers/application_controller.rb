dbab7801e30e22257711251f4a36e51639a242c3
Dan Lien <dan@danclien.com>
2011-10-14 21:00:14 -0500

    Updated album to Friday Night Hacks 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def get_photos
-    @photos = Picasa::Photo.find_all_by_album_id('danclien', '5637559171581951569')
+    @photos = Picasa::Photo.find_all_by_album_id('danclien', '5663531774001993713')
   end  
 end
